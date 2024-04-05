950b35c9e8c6f4905cb2b34fcb2947837b64d600
Sonal <sonalambekar99@gmail.com>
2024-04-05 17:22:06 +0530

    LangServDemo

---
+++ client.py
@@ -0,0 +1,26 @@
+import requests
+import streamlit as st
+
+def get_openai_response(input_text):
+    response=requests.post("http://localhost:8080/essay/invoke",
+    json={'input':{'topic':input_text}})
+
+    return response.json()['output']['content']
+
+def get_ollama_response(input_text1):
+    response=requests.post("http://localhost:8080/poem/invoke",
+    json={'input':{'topic':input_text1}})
+
+    return response.json()
+
+    ## streamlit framework
+
+st.title('Langchain Demo With LLAMA2 API')
+input_text=st.text_input("Write an essay on")
+input_text1=st.text_input("Write a poem on")
+
+if input_text:
+    st.write(get_openai_response(input_text))
+
+if input_text1:
+    st.write(get_ollama_response(input_text1))
